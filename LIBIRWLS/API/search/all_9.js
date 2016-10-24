286679b9bda24ada6996b7ad64c59f42a9c5fc3a
RobeDM <rober.diaz@gmail.com>
2016-10-24 02:56:14 +0200

    New API

--- LIBIRWLS/API/search/all_9.js
+++ LIBIRWLS/API/search/all_9.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['libirwls',['LIBIRWLS',['../index.html',1,'']]],
   ['l',['l',['../d0/d6e/structsvm__dataset.html#a899bdc43cdfb5e7063e8d7d1787357b4',1,'svm_dataset']]],
   ['labels',['Labels',['../d0/d38/structpredictProperties.html#a580276e3e15305d5ac16d9ce0eb9b20a',1,'predictProperties']]],
   ['libirwls_2dpredict_2ec',['LIBIRWLS-predict.c',['../df/d6f/LIBIRWLS-predict_8c.html',1,'']]],
